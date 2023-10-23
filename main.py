4901051a6266a91c4a387f431a29c1248c58e085
Nikita Krylov <nikita-krylov-2012@mail.ru>
2023-10-23 11:36:10 +0300

    Adding calculate numbers to html page

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])                #Decorator
+@app.route("/")                                 #Decorator
 def main():                                     #Creating function
-    return "<h1>Hello my dear students!</h1>"   #h1 - first header in HTML
+    return "<h1>Hello my dear students!</h1>"\
+           "<a href='/info'>About me</a><br>"\
+           "<a href = '/calc/1/5'>Calculate</a>"
+
+@app.route("/info")                             #Decorator
+def info():                                     #Creating function
+    return "<b>Creating by Krylov</b>"          #b - bold font
+
+@app.route("/calc/<x>/<y>")
+def calc(x, y):
+    return f"{x} + {y} = {int(x) + int(y)}"
 
 
 if __name__ == "__main__":
