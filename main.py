baa216e85b81d3c6a944fe43cf6abe31880be41f
Nikita Krylov <nikita-krylov-2012@mail.ru>
2023-10-23 11:07:18 +0300

    Starting first html page on server localhost

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-
+@app.route("/", methods=['GET'])                #Decorator
+def main():                                     #Creating function
+    return "<h1>Hello my dear students!</h1>"   #h1 - first header in HTML
 
 
 if __name__ == "__main__":
